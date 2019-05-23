5e6c7adfb933618f94b2eb0a1bc2c6cd899868c5
Feng Xiong <feng.xiong@student.tdsb.on.ca>
2019-05-23 19:28:04 -0400

    Reset Game file to last commit

--- Game.java
+++ Game.java
@@ -1,92 +1,138 @@
 import java.awt.Canvas;
 import java.awt.Graphics;
 import javax.swing.JFrame;
+import javax.swing.Timer;
 import java.awt.event.*;
 import java.awt.image.*;
 import java.awt.*;
 import javax.imageio.ImageIO;
 import java.io.*;
+import java.util.*;
 
-public class MainWindow extends JFrame implements ExitListener{
-  Game gameScreen;
-  String curScreen;
-  int size;
 
-  public MainWindow(){
-    super("The Slump");
-    curScreen = "menu";
-    size = Math.min((int)Toolkit.getDefaultToolkit().getScreenSize().getHeight(), (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth())-10;
-    setSize(size, size);
-    addMouseListener(new MouseAdapter() { 
-      public void mousePressed(MouseEvent me){
-        int x = me.getX();
-        int y = me.getY();
-        if(curScreen.equals("menu")){
-          if(x>290 && y> 130 && x < 623 && y > 262)
-            newGame();
-        }else if(curScreen.equals("level")){
-          openLevel();
-        }else if(curScreen.equals("highScore")){
-          openHighScore();
-        }else if(curScreen.equals("resume")){
-          resumeGame();
-        }else if(curScreen.equals("newGame")){
-         newGame();
-        }
-      } 
-    });
-    setLocationRelativeTo(null);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setVisible(true);
-    repaint();
+public class Game extends Canvas implements ActionListener{
+ ArrayList<Platform> platforms;
+ Timer timer;
+ boolean gameOver;
+ BufferedImage background;
+ BufferedImage canvas;
+ Player player;
+ int canvasX;
+ int canvasY;
+ int moveX;
+ int moveY;
+ ExitListener el;
+
+ public Game(int level, ExitListener el){
+  super();
+  setSize(400, 400);
+  canvasX = 0;
+  canvasY = 0;
+  addKeyListener(new PlayerListener());
+  canvas = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);
+  platforms = new ArrayList<Platform>();
+  gameOver = false;
+  moveX = 0;
+  this.el = el;
+  timer = new Timer(20, this);
+  timer.setInitialDelay(30);
+  timer.start();
+  if(level == 1){
+   level1();
+  }else if(level == 2){
+   level2();
+  }else{
+   level3();
   }
-  
-  public void openMenu(){
-    curScreen = "menu";
-    repaint();
+ }
+
+ public void level1(){
+  setSpawn(100, 200);
+  try{background = ImageIO.read(new File("platform.png"));}catch(IOException e){}//Remember to add the actual background
+  platforms.add(new Platform(0, 380, 400, 0, 0));
+  platforms.add(new Platform(190, 340, 30, 0, 0));
+  platforms.add(new Platform(190, 280, 30, 0, 0));
+  platforms.add(new Platform(190, 220, 30, 0, 0));
+  platforms.add(new Platform(0, 160, 0, 0, 0));
+ }
+
+ public void level2(){}
+
+ public void level3(){}
+
+ public void setSpawn(int x, int y){
+  player = new Player(x, y, 32, 102);
+ }
+
+ public void paint(Graphics g){
+  update(g);
+ }
+
+ public void update(Graphics g){
+  Graphics g1 = canvas.getGraphics();//Draw the graphics on a separate picture so that we can add pictures without flickering
+  //RedrawBackround
+  g1.setColor(new Color(0, 0, 0));
+  g1.fillRect(-100, -100, 1000, 1000);
+  g1.drawImage(background, 0, 0,1000, 1000, null);
+  //Update all items on screen
+  for(Platform i : platforms){
+   i.update(g1);//Updates to a new position
   }
-  
-  public void openLevel(){
-    curScreen = "level";
-    repaint();
+  player.move(moveX);
+  player.update(g1, platforms);
+  if(player.getX()+canvasX<getWidth()*0.25){
+   canvasX+=2;
   }
-  
-  public void openHighScore(){
-    curScreen = "highScore";
-    repaint();
+  if(player.getX()+canvasX>getWidth()*0.75){
+   canvasX-=2;
+  }
+  if(player.getY()+canvasY<getHeight()*0.25){
+   canvasY+=2;
+  }
+  if(player.getY()+canvasY>getHeight()*0.75){
+   canvasY-=2;
   }
 
-  public void resumeGame(){
-    add(gameScreen);
+  g.drawImage(canvas, canvasX, canvasY, null);
+ }
+
+ public void actionPerformed(ActionEvent e) {
+  //update new positions
+  if(!gameOver)
+   timer.restart();
+  else{
+   timer.stop();
+  }
+  repaint();
+ }
+
+ class PlayerListener extends KeyAdapter{
+
+  @Override
+  public void keyPressed(KeyEvent event){
+   int ch = event.getKeyCode();//Keep track of key presses
+   if(ch == KeyEvent.VK_UP || ch == KeyEvent.VK_W){
+    player.jump(10);
+   }else if(ch == KeyEvent.VK_LEFT|| ch == KeyEvent.VK_A){
+    moveX = -3;
+   }else if(ch == KeyEvent.VK_RIGHT || ch == KeyEvent.VK_D){
+    moveX = 3;
+   }
+   if(ch == KeyEvent.VK_ESCAPE){
+    el.exit();
+   }
   }
 
-  public void newGame(){
-    gameScreen = new Game(1, this);
-    add(gameScreen);
-    gameScreen .requestFocusInWindow(); 
-    pack();
+  @Override
+  public void keyReleased(KeyEvent event){
+   int ch = event.getKeyCode();//Keep track of key presses
+   if(ch == KeyEvent.VK_LEFT|| ch == KeyEvent.VK_A){
+    if(moveX != 3)
+     moveX = 0;
+   }else if(ch == KeyEvent.VK_RIGHT || ch == KeyEvent.VK_D){
+    if(moveX!=-3)
+    moveX = 0;
+   }
   }
-
-  public void exit(){
-    remove(gameScreen);
-    curScreen = "menu";
-  }
-
-  public void paint(Graphics g){
-    g.setColor(new Color(0, 0, 0));
-    g.fillRect(0, 0, getWidth(), getHeight());
-    if(curScreen.equals("menu")){
-      try {
-        BufferedImage background = ImageIO.read(new File("Screens/Menu.png"));
-        g.drawImage(background, 0, 0, size, size, null);
-      } catch (IOException e) {
-      }
-    }else if(curScreen.equals("level")){
-    }
-  }
-
-  public static void main(String[]args){
-    new MainWindow();
-  }
-  
+ }
 }
