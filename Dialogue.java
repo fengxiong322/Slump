7851e603435477c008c3b74518729ddccd3853df
rhapsody7893 <47641961+rhapsody7893@users.noreply.github.com>
2019-05-23 14:22:13 -0400

    Create Dialogue.java
    dis da words u say

---
+++ Dialogue.java
@@ -0,0 +1,17 @@
+public class Dialogue
+{
+  private String text;
+  
+  public Dialogue (String words)
+  {
+    text = words;
+  }
+  
+  public void display ()
+  {
+  }
+  
+  public void close ()
+  {
+  }
+}
