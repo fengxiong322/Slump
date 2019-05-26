873d0250449bf4fd2d3816fc8f73dff49da8a889
Feng Xiong <feng.xiong@student.tdsb.on.ca>
2019-05-26 12:06:36 -0400

    Attempt to create a system to find the highscores file

---
+++ Generator.java
@@ -0,0 +1,3 @@
+public class Generator extends Obstacle{
+	
+}
--- HighScore.java
+++ HighScore.java
@@ -1,7 +1,6 @@
 import java.awt.Canvas;
 import java.awt.Graphics;
-import javax.swing.JFrame;
-import javax.swing.Timer;
+import javax.swing.*;
 import java.awt.event.*;
 import java.awt.image.*;
 import java.awt.*;
@@ -11,7 +10,9 @@ import java.util.*;
 
 public class HighScore extends Canvas{
 	BufferedImage background;
-	File information;
+	String[] lines;
+	String path;
+	BufferedReader br;
 
 	public HighScore(){
 		try{
@@ -19,7 +20,36 @@ public class HighScore extends Canvas{
 		}catch(IOException e){
 			e.printStackTrace();
 		}
-		repaint();
+		boolean completed = false;
+		path = "";
+		try{
+			br = new BufferedReader(new FileReader(path));
+			completed = true;
+		}catch(IOException e){
+			e.printStackTrace();
+			JDialog dialog = new JDialog();
+			JPanel error = new JPanel();
+			dialog.setSize(300, 150);
+			JLabel prompt = new JLabel("Choose a file path to save your highscores:");
+			JTextField textField = new JTextField(20);
+			error.add(prompt);
+			error.add(textField);
+			JButton submit = new JButton("Submit");
+			submit.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent ae){
+					path = textField.getText();
+					try{
+						br = new BufferedReader(new FileReader(path));
+					}catch(IOException e){
+						textField.setText("That path does not exist!");
+					}
+				}
+			});
+			error.add(submit);
+			dialog.add(error);
+			dialog.setLocationRelativeTo(null);
+			dialog.setVisible(true);
+		}
 	}
 
 
@@ -27,5 +57,13 @@ public class HighScore extends Canvas{
 
 	}
 
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("test");
+		frame.setSize(300, 300);
+		frame.add(new HighScore());
+		frame.setLocationRelativeTo(null);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+	}
 
 }
