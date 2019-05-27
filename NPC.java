d364e2bcac53320b6b0baa7572aa3a81b35d983c
rhapsody7893 <47641961+rhapsody7893@users.noreply.github.com>
2019-05-27 14:03:51 -0400

    Update NPC.java

--- NPC.java
+++ NPC.java
@@ -21,6 +21,7 @@ import java.io.*;
 public class NPC extends Obstacle {
   String dialogue;
   
+  //May 24, Michael created constructor for class
   public NPC (int x, int y, int xSize, int ySize, BufferedImage image, String text) //added constuctor
   {
     super (x, y, xSize, ySize);
@@ -28,11 +29,13 @@ public class NPC extends Obstacle {
     dialogue = text;
   }
   
+  //May 24, Michael created basic method outiline 
   public void speek () //added speek method for testing
   {
     System.out.print (dialogue);
   }
   
+  //May 24, Michael implemented method
   public void update (Graphics g) //implemented update
   {
     g.drawImage(getImage(), getX(), getY(), getX() + getXSize(), getY() + getYSize(), 5, 5, 90, 90, null);
