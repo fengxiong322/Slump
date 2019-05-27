ed28e63ad65c6be8fb7d69f943072592f25a75ec
Square-1 <41208984+fengxiong322@users.noreply.github.com>
2019-05-27 14:26:16 -0400

    Update HighScore.java

--- HighScore.java
+++ HighScore.java
@@ -12,12 +12,14 @@ import java.util.*;
 /**
 * @author Feng
 * @version 2.0
-* Total time spent Feng: 0.5 hours
-
+* Total time spent Feng: 1 hour
+* Modifications: Added file management 1 hour May 26
 */
 public class HighScore extends Canvas{
 	BufferedImage background;
-	File information;
+	String[] lines;
+	String path;
+	BufferedReader br;
 
 	public HighScore(){
 		try{
@@ -25,7 +27,36 @@ public class HighScore extends Canvas{
 		}catch(IOException e){
 			e.printStackTrace();
 		}
-		repaint();
+		boolean completed = false;
+		path = "";
+		try{
+			br = new BufferedReader(new FileReader(path));
+			completed = true;
+		}catch(IOException e){
+			e.printStackTrace();
+			JDialog dialog = new JDialog();
+			JPanel error = new JPanel();
+			dialog.setSize(300, 150);
+			JLabel prompt = new JLabel("Choose a file path to save your highscores:");
+			JTextField textField = new JTextField(20);
+			error.add(prompt);
+			error.add(textField);
+			JButton submit = new JButton("Submit");
+			submit.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent ae){
+					path = textField.getText();
+					try{
+						br = new BufferedReader(new FileReader(path));
+					}catch(IOException e){
+						textField.setText("That path does not exist!");
+					}
+				}
+			});
+			error.add(submit);
+			dialog.add(error);
+			dialog.setLocationRelativeTo(null);
+			dialog.setVisible(true);
+		}
 	}
 
 
@@ -33,5 +64,13 @@ public class HighScore extends Canvas{
 
 	}
 
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("test");
+		frame.setSize(300, 300);
+		frame.add(new HighScore());
+		frame.setLocationRelativeTo(null);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+	}
 
 }
