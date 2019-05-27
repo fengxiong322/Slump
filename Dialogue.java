eafb6937fb9a446689f039da7327219047e0e2a8
rhapsody7893 <47641961+rhapsody7893@users.noreply.github.com>
2019-05-26 21:50:24 -0400

    Update Dialogue.java

--- Dialogue.java
+++ Dialogue.java
@@ -1,3 +1,11 @@
+/**
+* @author Feng, Michael
+* @version 2.0
+* Total time spent Feng: 0 hours
+* Total time spend Michael: 5 mins
+* Modifications: May 24, 2019, Michael Zhou, Total time: 4 mins
+* basic class structure, not sure if needed, here for testing purposes
+*/
 public class Dialogue
 {
   private String text;
