49aa9848c81cbd3916762b316c4f306bbee15440
Square-1 <41208984+fengxiong322@users.noreply.github.com>
2019-05-30 12:41:51 -0400

    Create InvisPlatform.java

---
+++ InvisPlatform.java
@@ -0,0 +1,21 @@
+import java.awt.Canvas;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.Timer;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.util.*;
+
+public class InvisPlatform extends Platform{
+  public InvisPlatform(int x, int y, int length){
+    super(x, y, length);
+  }
+  
+  public void update(Graphics g){
+    if(Player.getBounds().intersects(new Rectangle(getX()-50, getY()-50, getXSize()+100, getYSize()+100)))
+		  g.drawImage(getImage(), getX(), getY(), getXSize(), getYSize(), 5, 5, 90, 90, null);
+	}
+}
