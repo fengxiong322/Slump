a576d989d4e547aa55fb82dcf9b34fe7f94c12f3
Feng Xiong <feng.xiong@student.tdsb.on.ca>
2019-06-03 18:26:10 -0400

    Temp npc fix

--- Player.java
+++ Player.java
@@ -154,9 +154,7 @@ public class Player{
    xSpeed = 0;
   }
   for(Obstacle i : platforms.find(x+xSpeed, y+ySpeed, sizeX, sizeY)){
-    if(i instanceof Door || i instanceof StateSwitchPlatform && !((StateSwitchPlatform)i).isOn() ||i instanceof Projectile)
-      continue;
-    if(i instanceof StateSwitchPlatform && !((StateSwitchPlatform)i).isOn())
+    if(i instanceof Door || i instanceof StateSwitchPlatform && !((StateSwitchPlatform)i).isOn() ||i instanceof Projectile || i instanceof NPC)
       continue;
    Rectangle platformRect = new Rectangle(i.getX(), i.getY(), i.getXSize(), i.getYSize());
    playerRect = new Rectangle(x+xSpeed, y+ySpeed, sizeX, sizeY);
