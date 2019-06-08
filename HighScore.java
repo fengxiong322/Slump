f02f914f199ef5138825a293c963b6c7baffbe56
Feng Xiong <feng.xiong@student.tdsb.on.ca>
2019-06-08 16:27:59 -0400

    Update HighScore.java

--- HighScore.java
+++ HighScore.java
@@ -21,26 +21,52 @@ public class HighScore extends Canvas{
 	String path;
 	BufferedReader br;
 	ExitListener el;
+	File information;
 
 	public HighScore(ExitListener el){//Added constructor Feng May 22 1 hour
 		//Reading files Feng May 22 30 min
 		setSize(811, 821);
 		this.el = el;
+		addMouseListener(new MouseAdapter(){
+      public void mousePressed(MouseEvent me){
+        int x = me.getX();
+        int y = me.getY();
+        //added coord detection for testing
+        System.out.println(x + " " + y);
+        //added navigation between screens - Michael Zhou, May 24, 2019 5mins
+          if(x>344 && y> 691 && x < 598 && y < 320)
+          {
+          	el.exit();
+          }
+      }
+		});
 		try{
 			background = ImageIO.read(new File("Screens/Highscores.jpg"));//Change this picture
 		}catch(IOException e){
 			e.printStackTrace();
 		}
 		//Create directory
-		File information = new File(System.getProperty("user.home") + "/slump/highscores.txt");
-		//if(information.createNewFile())
-			System.out.println("new file created");
-
-
+		File directory = new File(System.getProperty("user.home") + "/slump");
+		information = new File(System.getProperty("user.home") + "/slump/highscores.txt");
+		try{
+			directory.mkdirs();
+			if(information.createNewFile())
+				System.out.println("file create");
+			else
+				System.out.println("File already exists");
+	}catch(IOException e){
+	}
 	}
 
-
 	public void paint(Graphics g){//Created empy paint method Feng 20min
+		g.drawImage(background, 0, 0, 811, 821, null);
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(information));
+			for(int i = 100; i < 800; i+=100){
+				g.drawString(br.readLine(), 20, i);
+				g.drawString(br.readLine(), 400, i);
+			}
+		}catch(IOException e){}
 
 	}
 }
